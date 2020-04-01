87e5d86bfe4af58c009c6ba5df4159e2148e2622
janda <rostislav.janda@fio.cz>
2020-04-01 13:57:36 +0200

    Discard shows larger portion of the cards

--- src/components/cards/DiscardDeck.js
+++ src/components/cards/DiscardDeck.js
@@ -5,7 +5,7 @@ import Card from "./Card";
 export const DiscardDeck = (props) => {
     return (
     props.cards.map((card, i) => {
-        const margin = 5 + i * 10;
+        const margin = 5 + i * 80;
         return (
             <div className="align-middle" key={i}>
                 <div style={{position: "absolute", marginLeft: margin, marginTop: 5}}>
