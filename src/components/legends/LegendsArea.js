eae374e4a61c5bae59a4592e7245101055b1ffa5
janda <rostislav.janda@fio.cz>
2020-04-19 18:05:43 +0200

    Expedition placeholder element added to Legends area

--- src/components/legends/LegendsArea.js
+++ src/components/legends/LegendsArea.js
@@ -2,6 +2,8 @@ import React, {useContext} from 'react';
 import {BoardStateContext} from "../../Contexts";
 import {Legend} from "./Legend";
 import {Income} from "./tiles/Income";
+import goal from  "../../img/cardBackgrounds/Goal.png"
+import bonusBgr from "../../img/bonus_action_background.png";
 
 export function LegendsArea() {
     const boardStateContext = useContext(BoardStateContext);
@@ -32,6 +34,17 @@ export function LegendsArea() {
         marginLeft: "16.5vw"
     }
 
+    const explorePlaceholder = <div style={{
+        height: "3.5vw",
+        width: "5vw",
+        backgroundImage: `url(${goal}`,
+        backgroundSize: "100% 100%",
+        fontSize: "0.9vw",
+        color: "white",
+        float: "left",
+    }}>
+    </div>
+
     return (
         <div style={containerStyle}>
             {legends && legends.map((legend, i) =>
@@ -39,11 +52,13 @@ export function LegendsArea() {
                     <Legend legend={legend} legends={legends} legendIndex={i}/>
                 </div>
             )}
+            <div style={{marginLeft:"0.5vw"}}>{explorePlaceholder}</div>
             {incomes1offer && incomes1offer.map((income, i) =>
                 <div style={incomes1Style} key={i}>
                     <Income income={income}/>
                 </div>
             )}
+            <div style={{position: "absolute", marginLeft:"11.5vw"}}>{explorePlaceholder}</div>
             {incomes1offer && incomes2offer.map((income, i) =>
                 <div style={incomes2Style} key={i}>
                     <Income income={income}/>
