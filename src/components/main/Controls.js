c25b3cc684b30a4f654f56de09aa8f45c58af2c8
janda <rostislav.janda@fio.cz>
2020-04-25 12:22:38 +0200

    Controls layout modified

--- src/components/main/Controls.js
+++ src/components/main/Controls.js
@@ -3,6 +3,7 @@ import {PlayerStateContext} from "../../Contexts";
 import {socket} from "../../server/socketConnection";
 import {TRANSMISSIONS} from "../../data/idLists";
 import {useHistory} from "react-router-dom";
+import {ButtonGroup} from "react-bootstrap";
 
 export const Controls = (props) => {
     const playerStateContext = useContext(PlayerStateContext);
@@ -18,18 +19,24 @@ export const Controls = (props) => {
         left: "47vw",
         display: "flex",
         flexFlow: "row",
-        alignItems: "baseline"
+        alignItems: "baseline",
     }
 
     const history = useHistory();
     return (
         <div style={containerStyle}>
-            <button className="btn-primary" onClick={() => playerStateContext.nextPlayer()}>next player</button>
-            <button className="btn-primary" onClick={() => playerStateContext.handleEndRound()}>end of  round</button>
-            <button className="btn-primary" onClick={() => restartGame()}>restart game</button>
-            <button className="btn-primary"
-                    onClick={() => history.push({pathname: "/scoring", data: playerStateContext.playerState})}>scoring
-            </button>
+            <ButtonGroup aria-label="Control buttons">
+                <button className="btn-primary" onClick={() => playerStateContext.nextPlayer()}>next player</button>
+                <button className="btn-primary" onClick={() => playerStateContext.handleEndRound()}>end of round
+                </button>
+                <button className="btn-primary" onClick={() => restartGame()}>restart game</button>
+                <button className="btn-primary"
+                        onClick={() => history.push({
+                            pathname: "/scoring",
+                            data: playerStateContext.playerState
+                        })}>scoring
+                </button>
+            </ButtonGroup>
             <div style={{display: "flex", flexFlow: "row", justifyContent: "space-evenly", marginLeft: "1vw"}}>
                 {playerStateContext.playerState.activeEffects[0]}
                 {isActivePlayer ? <p>Your turn! Actions: {playerStateContext.playerState.actions}</p> :
