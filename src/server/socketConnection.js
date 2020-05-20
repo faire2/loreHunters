6847a562ce248e1056aa399608d81ea6797a1ef7
janda <rostislav.janda@fio.cz>
2020-05-20 10:12:44 +0200

    socket address changed to arnak

--- src/server/socketConnection.js
+++ src/server/socketConnection.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client"
 
 /* for production / devel */
-export const socket = io(process.env.NODE_ENV === "development" ? "localhost:4001" : "https://lore-hunters.herokuapp.com");
+export const socket = io(process.env.NODE_ENV === "development" ? "localhost:4001" : "https://arnak.herokuapp.com");
