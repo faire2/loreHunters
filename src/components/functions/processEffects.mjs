00af01c3bde2b5ff7c3a01fe4eecb3129055461d
janda <rostislav.janda@fio.cz>
2020-09-01 09:03:01 +0200

    Bugfix: lost city effects doesn't crash in modal

--- src/components/functions/processEffects.mjs
+++ src/components/functions/processEffects.mjs
@@ -152,10 +152,10 @@ export function processEffects(tCard, cardIndex, originalPlayersState, effects, 
                 case EFFECT.discard:
                     if (tPlayerState.hand.length > 0) {
                         tActiveEffects.push(effect);
-                        let tEffects = [];
                         const discardIndex = effects.indexOf(EFFECT.discard);
                         // if we have more effects to process, we hide them to active effects
                         if (discardIndex < effects.length - 1) {
+                            let tEffects = [];
                             for (let i = discardIndex + 1; i < effects.length; i++) {
                                 tEffects.push(effects[i]);
                             }
@@ -214,9 +214,13 @@ export function processEffects(tCard, cardIndex, originalPlayersState, effects, 
                 // additionaly he can choose from array of effects set on legend initialization
                 case EFFECT.discoverLostCity:
                     tPlayerState.canActivateLostCity = true;
+                    let effectsArr = [];
+                    for (let effect of tLegend.lostCityEffects) {
+                        effectsArr.push([effect]);
+                    }
                     rewardsData = {
                         type: REWARD_TYPE.effectsArr,
-                        data: tLegend.lostCityEffects,
+                        data: effectsArr,
                     };
                     showRewardsModal = true;
                     break;
